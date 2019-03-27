29999589577eade40dfe5c83f9005def2678dc85
scottdy <42708967@qq.com>
2019-03-27 22:44:28 +0800

    end stash

--- src/main/java/gitdemo/Main.java
+++ src/main/java/gitdemo/Main.java
@@ -5,16 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, git!");
-		System.out.println("make runable");
-		System.out.println("back to master change");
+	
 		System.out.println("add hot_fix");
 		System.out.println("add hot_fix2");
 		System.out.println("update by yadu");
 		System.out.println("add hot_fix merge");
-		
-		System.out.println("continue fix work");
-		
 
+		System.out.println("continue fix work");
+
+		System.out.println("start stash");
 
 	}
 
