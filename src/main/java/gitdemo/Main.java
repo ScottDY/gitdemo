bda1a9cab83385afd414c92bcb96ed92a691946a
scottdy <42708967@qq.com>
2019-04-01 17:58:58 +0800

    make jar with dependencies

--- src/main/java/gitdemo/Main.java
+++ src/main/java/gitdemo/Main.java
@@ -1,19 +1,19 @@
 package gitdemo;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, git!");
-	
-		System.out.println("add hot_fix");
-		System.out.println("add hot_fix2");
-		System.out.println("update by yadu");
-		System.out.println("add hot_fix merge");
+		String text1 = "a12340";
+		String text2 = "1234";
 
-		System.out.println("continue fix work");
+		boolean result1 = StringUtils.isNumeric(text1);
+		boolean result2 = StringUtils.isNumeric(text2);
 
-		System.out.println("start stash");
+		System.out.println(text1 + " is a numeric? " + result1);
+		System.out.println(text2 + " is a numeric? " + result2);
 
 	}
 
